d204ebb1e15d5be2b296f18fb036c034ae3733c7
Kulbhushan Sharma <sharma.kul.1982@gmail.com>
2020-09-21 18:42:40 -0600

    missed update view.py. deploying now

--- coronaDash/firstPage/views.py
+++ coronaDash/firstPage/views.py
@@ -1,98 +1,105 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 import pandas as pd
-import numpy as np
+import json
+
+# Read json file
+globalMap = pd.read_json(
+    'https://cdn.jsdelivr.net/gh/highcharts/highcharts@v7.0.0/samples/data/world-population-density.json')
+
+
+# globalMap = pd.read_json('/static/firstUI/world-population-density.json')
+
+# globalMap_data = open('/static/firstUI/world-population-density.json')
+# globalMap_data_clean = json.load(globalMap_data)
+# globalMap = json.dumps(globalMap_data_clean)
+# globalMap_data.close()
+
 # Create your views here.
-df3=pd.read_json('https://cdn.jsdelivr.net/gh/highcharts/highcharts@v7.0.0/samples/data/world-population-density.json')
 
-def index(request):
-    confirmedGlobal=pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv',encoding='utf-8',na_values=None)
-    # deathGLobal=pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv')
-    # recoverGlobal=pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv')
-    uniqueCountryNames=pd.unique(confirmedGlobal['Country/Region'])
-    contryNames,countsVal,logVals,overallCount,dataForMapGraph,maxVal=getBarData(confirmedGlobal,uniqueCountryNames)
-    dataForheatMap,dateCat=getHeatMapData(confirmedGlobal,contryNames)
-    datasetForLine,axisvalues=getLinebarGroupData(confirmedGlobal,uniqueCountryNames)
-    context={'dateCat':dateCat,'dataForheatMap':dataForheatMap,'maxVal':maxVal,'dataForMapGraph':dataForMapGraph,'axisvalues':axisvalues,'datasetForLine':datasetForLine,'uniqueCountryNames':uniqueCountryNames,'contryNames':contryNames,'countsVal':countsVal,'logVals':logVals,'overallCount':overallCount}
-    return render(request,'index.html',context)
-    
 
-def getBarData(confirmedGlobal,uniqueCountryNames):
-    df2=confirmedGlobal[list(confirmedGlobal.columns[1:2])+list([confirmedGlobal.columns[-2]])]
-    df2.columns=['Country/Region','values']
-    df2=df2.sort_values(by='values',ascending=False)
-    contryNames=list(df2['Country/Region'].values)
-    countsVal=list(df2['values'].values)
-    maxVal=max(countsVal)
-    overallCount=sum(countsVal)
-    logVals=list(np.log(ind) if ind != 0 else 0 for ind in countsVal )
-    dataForMapGraph=getDataforMap(uniqueCountryNames,df2)
-    # dictVal=[]
-    # for i in range(df2.shape[0]):
-    #     dictVal.append(dict(df2.ix[i]))
-    return (contryNames,countsVal,logVals,overallCount,dataForMapGraph,maxVal)
+def indexPage(request):
+    confirmedGlobal = pd.read_csv(
+        'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv', encoding='utf-8', na_values=None)
 
-def getLinebarGroupData(confirmedGlobal,uniqueCountryNames):
-    colNames=confirmedGlobal.columns[4:-1]
-    datasetsForLine=[]
-    for i in uniqueCountryNames:
-        temp={}
-        temp['label']=i
-        temp['fill']='false'
-        temp['data']=confirmedGlobal[confirmedGlobal['Country/Region']==i][colNames].sum().values.tolist()
-        datasetsForLine.append(temp)
-    return datasetsForLine,list(range(len(colNames)))
+    overallCount = confirmedGlobal[confirmedGlobal.columns[-1]].sum()
+    barPlotData = confirmedGlobal[['Country/Region', confirmedGlobal.columns[-1]]].groupby(
+        'Country/Region').sum().sort_values(by=confirmedGlobal.columns[-1], ascending=False)
+    barPlotData = barPlotData.reset_index()
+    barPlotData.columns = ['Country/Region', 'values']
+    barPlotData = barPlotData.sort_values(by='values', ascending=False)
+    # countryNames = barPlotData['Country/Region'].values.tolist()
+    # countsVal = barPlotData['values'].values.tolist()
+    # dataForMap = mapDataCal(barPlotData, countryNames)
 
-def getDataforMap(uniqueCOuntryName,df2):
-    dataForMap=[]
-    for i in uniqueCOuntryName:
+    # print("time_series_covid19_deaths_global..")
+    covid19_deaths_global = pd.read_csv(
+        'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv', encoding='utf-8', na_values=None)
+    overalldeathCount = covid19_deaths_global[covid19_deaths_global.columns[-1]].sum()
+    barPlotDeathData = covid19_deaths_global[['Country/Region', covid19_deaths_global.columns[-1]]].groupby(
+        'Country/Region').sum().sort_values(by=covid19_deaths_global.columns[-1], ascending=False)
+    barPlotDeathData = barPlotDeathData.reset_index()
+    barPlotDeathData.columns = ['Country/Region', 'values']
+    barPlotDeathData = barPlotDeathData.sort_values(
+        by='values', ascending=False)
+
+    barPlotData = barPlotData.merge(barPlotDeathData, left_on='Country/Region',
+                                    right_on='Country/Region')
+    barPlotData.columns = ['Country/Region', 'valuesx', 'valuesy']
+
+    barPlotData.loc[barPlotData['Country/Region']
+                    == 'US', 'Country/Region'] = 'United States'
+    barPlotData.loc[barPlotData['Country/Region'] ==
+                    'Russia', 'Country/Region'] = 'Russian Federation'
+
+    barPlotData.loc[barPlotData['Country/Region'] ==
+                    'Iran', 'Country/Region'] = 'Iran, Islamic Rep.'
+
+    barPlotData.loc[barPlotData['Country/Region'] ==
+                    'Egypt', 'Country/Region'] = 'Egypt, Arab Rep.'
+
+    barPlotData.loc[barPlotData['Country/Region'] ==
+                    'Yemen', 'Country/Region'] = 'Yemen, Rep.'
+
+    barPlotData.loc[barPlotData['Country/Region'] ==
+                    'Venezuela', 'Country/Region'] = 'Venezuela, RB'
+
+    barPlotData.loc[barPlotData['Country/Region'] ==
+                    'Congo (Brazzaville)', 'Country/Region'] = 'Congo, Dem. Rep.'
+
+    barPlotData.loc[barPlotData['Country/Region'] ==
+                    'Congo (Kinshasa)', 'Country/Region'] = 'Congo, Rep.'
+
+    barPlotData.loc[barPlotData['Country/Region'] ==
+                    'Korea, South', 'Country/Region'] = 'Korea, Rep.'
+
+    countryNames = barPlotData['Country/Region'].values.tolist()
+    countsVal = barPlotData['valuesx'].values.tolist()
+    deathcountsVal = barPlotData['valuesy'].values.tolist()
+    dataForMap = mapDataCal(barPlotData, countryNames)
+    # print("dataForMap: ", dataForMap)
+
+    # dataForMap = mapDataCal(barPlotDeathData, countryNames)
+    print("countsVal", countsVal)
+
+    context = {'overallCount': overallCount, 'countryNames': countryNames,
+               'countsVal': countsVal, 'deathcountsVal': deathcountsVal, 'overalldeathCount': overalldeathCount, 'deathcountsVal': deathcountsVal,
+               'dataForMap': dataForMap}
+
+    return render(request, 'firstUI/index.html', context)
+
+
+def mapDataCal(barPlotData, countryNames):
+    dataForMap = []
+    for i in countryNames:
         try:
-            tempdf=df3[df3['name']==i]
-            temp={}
-            temp["code3"]=list(tempdf['code3'].values)[0]
-            temp["name"]=i
-            temp["value"]=df2[df2['Country/Region']==i]['values'].sum()
-            temp["code"]=list(tempdf['code'].values)[0]
+            tempdf = globalMap[globalMap['name'] == i]
+            temp = {}
+            temp["code3"] = list(tempdf['code3'].values)[0]
+            temp["name"] = i
+            temp["value"] = barPlotData[barPlotData['Country/Region']
+                                        == i]['valuesx'].sum()
+            temp['code'] = list(tempdf['code'].values)[0]
             dataForMap.append(temp)
         except:
             pass
-    print (len(dataForMap))
     return dataForMap
-
-
-def getHeatMapData(confirmedGlobal,contryNames):
-    df3=confirmedGlobal[list(confirmedGlobal.columns[1:2])+list(list(confirmedGlobal.columns.values)[-6:-1])]
-    dataForheatMap=[]
-    for i in contryNames:
-        try:
-            tempdf=df3[df3['Country/Region']==i]
-            temp={}
-            temp["name"]=i
-            temp["data"]=[{'x':j,'y':k} for j,k in zip(tempdf[tempdf.columns[1:]].sum().index,tempdf[tempdf.columns[1:]].sum().values)]
-            dataForheatMap.append(temp)
-        except:
-            pass
-    dateCat=list(list(confirmedGlobal.columns.values)[-6:-1])
-    return dataForheatMap,dateCat
-
-
-def drillDownACountry(request):
-    print (request.POST.dict())
-    countryName=request.POST.get('countryName')
-    confirmedGlobal=pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv',encoding='utf-8',na_values=None)
-    countryDataSpe=pd.DataFrame(confirmedGlobal[confirmedGlobal['Country/Region']==countryName][confirmedGlobal.columns[4:-1]].sum()).reset_index()
-    countryDataSpe.columns=['country','values']
-    countryDataSpe['lagVal']=countryDataSpe['values'].shift(1).fillna(0)
-    countryDataSpe['incrementVal']=countryDataSpe['values']-countryDataSpe['lagVal']
-    countryDataSpe['rollingMean']=countryDataSpe['incrementVal'].rolling(window=4).mean()
-    countryDataSpe=countryDataSpe.fillna(0)
-    datasetsForLine=[{'yAxisID': 'y-axis-1','label':'Daily Cumulated Data','data':countryDataSpe['values'].values.tolist(),'borderColor':'#03a9fc','backgroundColor':'#03a9fc','fill':'false'},
-                    {'yAxisID': 'y-axis-2','label':'Rolling Mean 4 days','data':countryDataSpe['rollingMean'].values.tolist(),'borderColor':'#fc5203','backgroundColor':'#fc5203','fill':'false'}]
-    axisvalues=countryDataSpe.index.tolist()
-    uniqueCountryNames=pd.unique(confirmedGlobal['Country/Region'])
-    contryNames,countsVal,logVals,overallCount,dataForMapGraph,maxVal=getBarData(confirmedGlobal,uniqueCountryNames)
-    dataForheatMap,dateCat=getHeatMapData(confirmedGlobal,contryNames)
-    context=context={"countryName":countryName,'axisvalues':axisvalues,'datasetsForLine':datasetsForLine,'dateCat':dateCat,'dataForheatMap':dataForheatMap,'maxVal':maxVal,'dataForMapGraph':dataForMapGraph,'uniqueCountryNames':uniqueCountryNames,'contryNames':contryNames,'countsVal':countsVal,'logVals':logVals,'overallCount':overallCount}
-
-    return render(request,'index2.html',context)
-
